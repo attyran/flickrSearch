5dc6186f437c4ecd1f639e2a0d21ba45ae0c8074
alex tran <>
2019-09-01 13:53:20 -0400

    Cleaner code.

--- app/src/main/java/com/attyran/flickrsearch/FlickrAdapter.kt
+++ app/src/main/java/com/attyran/flickrsearch/FlickrAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.flickr_rv_item.view.*
 
 import java.util.ArrayList
 
@@ -42,11 +43,12 @@ class FlickrAdapter(private val mPhotos: List<Photo>) : RecyclerView.Adapter<Fli
 
         init {
             this.mPhotos = photos
-            mPhoto = itemView.findViewById(R.id.item_image)
+            mPhoto = itemView.item_image
             itemView.setOnClickListener(this)
         }
 
         override fun onClick(view: View) {
+            //todo
         }
     }
 }
