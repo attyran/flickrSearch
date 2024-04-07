b4e6652ad85960fd5e5919e7d02ced5c81fd3a86
Alex Tran <hoangcau.tran@gmail.com>
2024-04-06 20:59:31 -0700

    Clean warnings

--- app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
+++ app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
@@ -3,8 +3,6 @@ package com.attyran.flickrsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.attyran.flickrsearch.network.BackendService
 import kotlinx.coroutines.launch
