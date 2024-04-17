1a94874202066d0fc187d722fc025ceb25cc122d
Alex Tran <hoangcau.tran@gmail.com>
2024-04-17 14:47:17 -0700

    Add details screen

---
+++ app/src/main/java/com/attyran/flickrsearch/DetailsScreen.kt
@@ -0,0 +1,13 @@
+package com.attyran.flickrsearch
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.attyran.flickrsearch.network.Photo
+
+@Composable
+fun DetailsScreen(photoURL: String?) {
+    Column {
+        Text(text = "Details Screen")
+    }
+}
