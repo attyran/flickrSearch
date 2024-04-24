5b1cf9c1fb5a6217f9192dccfabd65ba66cc3a30
Alex Tran <hoangcau.tran@gmail.com>
2024-04-24 00:00:35 -0700

    Update

--- app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
+++ app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.attyran.flickrsearch
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
