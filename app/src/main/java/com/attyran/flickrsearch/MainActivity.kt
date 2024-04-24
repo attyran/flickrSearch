7bbded4616ef890453f729afe40b8789d371f09c
Alex Tran <hoangcau.tran@gmail.com>
2024-04-23 22:06:12 -0700

    Refactor

--- app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
+++ app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
@@ -3,16 +3,10 @@ package com.attyran.flickrsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.attyran.flickrsearch.network.BackendService
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var viewModel: FlickrViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        viewModel = FlickrViewModel(BackendService())
         setContent {
             FlickrNavGraph()
         }
