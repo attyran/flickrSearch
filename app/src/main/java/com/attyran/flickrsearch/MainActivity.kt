bd84f0fff5c3e91b3895465a50c1b7b291c10119
Alex Tran <hoangcau.tran@gmail.com>
2024-04-23 21:10:21 -0700

    Clear warnings

--- app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
+++ app/src/main/java/com/attyran/flickrsearch/MainActivity.kt
@@ -3,12 +3,7 @@ package com.attyran.flickrsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.attyran.flickrsearch.network.BackendService
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
