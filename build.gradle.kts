7f3711f8e18dec5f4efc8930e2ca2008d3abac3e
Alex Tran <hoangcau.tran@gmail.com>
2024-07-14 14:16:54 -0700

    Upgrade grade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.gradle.kotlin.dsl.extra
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
@@ -13,7 +12,7 @@ buildscript {
     }
     dependencies {
 
-        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("com.android.tools.build:gradle:8.5.1")
         val kotlinVersion: String by project
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
