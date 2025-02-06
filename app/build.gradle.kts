976757527ffc63e3f46bfcf2334bdb5086335855
Alex Tran <hoangcau.tran@gmail.com>
2025-02-05 20:11:50 -0800

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.attyran.flickrsearch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.attyran.flickrsearch"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
